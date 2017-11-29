79282df5b207bc1fd8485f7920035af3d745edd5
gcrowell <graham.crowell@gmail.com>
2017-11-29 01:14:36 -0800

    Jenkins

--- serverless_web_app/setup.sh
+++ serverless_web_app/setup.sh
@@ -4,17 +4,18 @@ echo "hello"
 
 BUCKET_NAME=treasure-app
 REGION=us-west-2
+ACCOUNT_NUMBER=584221209899
 
 #
-# S3 static website
+# 1_static_website
 #
 
 # STEP 2:
 # create new bucket
-aws s3api create-bucket --cli-input-json file://create_s3_bucket.json
+aws s3api create-bucket --create-bucket-configuration LocationConstraint=$REGION --cli-input-json file://1_static_website/create_s3_bucket.json
 # STEP 3:
 # set static public access policy
-aws s3api put-bucket-policy --bucket $BUCKET_NAME"-bucket" --policy file://s3_public_access_policy.json
+aws s3api put-bucket-policy --bucket $BUCKET_NAME"-bucket" --policy file://1_static_website/s3_public_access_policy.json
 # STEP 4:
 # clone git repo
 git clone https://github.com/awslabs/aws-serverless-workshops $BUCKET_NAME"-bucket"
@@ -36,16 +37,16 @@ open http://$BUCKET_NAME"-bucket".s3-website-$REGION.amazonaws.com
 # Create Cognito user pool
 aws cognito-idp create-user-pool --pool-name $BUCKET_NAME"-cognito-pool"
 # save pool id
-POOL_ID=us-west-2_NmXwnPKQw
+POOL_ID="us-west-2_yQDEFbVeC"
 # STEP 2:
 # add a client to the user pool
 aws cognito-idp create-user-pool-client --user-pool-id $POOL_ID --client-name $BUCKET_NAME"-cognito-client" --no-generate-secret
 # save client id
-CLIENT_ID=17n69utji52lmqesstrfo3ml5a
+CLIENT_ID="4cfhb8467jl6lc2kknrb0jumfn"
 # update (local) config.js with above ids
 subl treasure-app"-bucket"/WebApplication/1_StaticWebHosting/website/js/config.js
 # upload updated config.js (s3)
-aws s3 sync treasure-app-bucket/WebApplication/1_StaticWebHosting/website s3://$BUCKET_NAME"-bucket" --region $REGION
+aws s3 sync $BUCKET_NAME"-bucket"/WebApplication/1_StaticWebHosting/website s3://$BUCKET_NAME"-bucket" --region $REGION
 
 # TEST:
 # register to mock website
@@ -60,6 +61,34 @@ open http://$BUCKET_NAME"-bucket".s3-website-$REGION.amazonaws.com/register.html
 # Lambda on top of DynamoDB backend
 #
 # https://aws.amazon.com/getting-started/serverless-web-app/module-3/
+# STEP 1:
+# create table
+aws dynamodb create-table --table-name "Rides" --attribute-definitions AttributeName="RideId",AttributeType="S" --key-schema AttributeName="RideId",KeyType="HASH" --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10
+# save arn (amazon resource number)
+# arn:aws:dynamodb:region:account-id:table/table-name
+TABLE_ARN="arn:aws:dynamodb:us-west-2:584221209899:table/Rides"
+# STEP 2:
+# create iam role 
+aws iam create-role --role-name "WildRydesLambda" --assume-role-policy-document file://role_policy.json
+# save arn
+ROLE_ARN=arn:aws:iam::584221209899:role/WildRydesLambda
+ROLE_ID=AROAINII7J3ZCACT65LEI
+# STEP 3:
+# attach managed policies to role
+aws iam attach-role-policy  --role-name "WildRydesLambda" --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+aws iam attach-role-policy  --role-name "WildRydesLambda" --policy-arn arn:aws:iam::aws:policy/AWSLambdaFullAccess
+# STEP 4:
+# create lambda function
+aws lambda create-function --function-name "RequestUnicorn" --code file://serverless_web_app/$BUCKET_NAME"-bucket"/WebApplication/3_ServerlessBackend/requestUnicorn.js --role "WildRydesLambda" --runtime "nodejs6.10" --handler index.handler 
+# get s3 bucket
+BUCKET=$(aws s3api list-buckets | awk ' { print $3 } ')
+
+# http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/api-permissions-reference.html
+# https://gist.github.com/gene1wood/55b358748be3c314f956
+
+dynamodb:PutItem
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/ReadOnlyAccess --role-name ReadOnlyRole
+a
 
 
 #
